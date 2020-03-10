4642435b5e5e31f63a4d5a59bd555ef6cf956e1c
Vasu Eranki <vasueranki@gmail.com>
2020-03-10 16:13:59 +0530

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x_int = int(input())
+y_double = float(input())
+z_str = str(input())
+# Read and save an integer, double, and String to your variables.
+x_int +=4
+# Print the sum of both integer variables on a new line.
+y_double += d 
+# Print the sum of the double variables on a new line.
+s+= z_str
+print(x_int)
+print('%.1f'%y_double)
+print(s)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
