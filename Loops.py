3d4790bdd25aebd305b606f02fccfc7fa91dbd1a
Vasu Eranki <vasueranki@gmail.com>
2020-03-14 15:27:15 +0530

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -1,6 +1,5 @@
 #!/bin/python3
+
 if __name__ == '__main__':
     n = int(input())
-    for i in range(1,11):
-        temp = n*i
-        print(n,"x",i,"=",temp)
+    [print(n,"x",i,"=",n*i) for i in range(1,11)] 
