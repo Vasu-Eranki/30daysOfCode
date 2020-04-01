f0c6c932cbffd1f6f04340785e7fb0ce8fac1387
Vasu Eranki <vasueranki@gmail.com>
2020-04-01 19:03:24 +0530

    Create Interfaces.py

---
+++ Interfaces.py
@@ -0,0 +1,17 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        temp=1
+        for i in range(2,n+1):
+            if(n%i==0):
+                temp=temp+i
+        return temp
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
