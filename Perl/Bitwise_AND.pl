3a61fdaae639959f96175a82ac19b47800f118fe
Vasu Eranki <vasueranki@gmail.com>
2020-08-01 19:07:56 +0530

    Update Bitwise_AND.pl

--- Perl/Bitwise_AND.pl
+++ Perl/Bitwise_AND.pl
@@ -1,26 +1,16 @@
 #!/usr/bin/perl
-use List::Util qw(max);
 use strict;
 use warnings;
-my $t = <>;
-$t =~ s/\s+$//;
-
+my $t = <STDIN>;
+chomp $t;
 for (my $t_itr = 0; $t_itr < $t; $t_itr++) {
-    my $nk = <>;
+    my $nk = <STDIN>;
     chomp $nk;
     my @nk = split(" ",$nk);
     my $n = $nk[0];
-    chomp $n;
     my $k = $nk[1];
+    chomp $n;
     chomp $k;
-    my $ans=0;
-    if(($k|$k-1)<=$n)
-    {
-        $ans=$k-1;
-    }
-    else
-    {
-        $ans=$k-2;
-    }
+    my $ans=(($k|$k-1)<=$n)?$k-1:$k-2;
     print("$ans\n");
 }
